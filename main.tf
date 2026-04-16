744aa52de433136537e017b55299cc4a63225703
Avorlen <48469505+Avorlen@users.noreply.github.com>
2026-04-16 20:00:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-user1" #change here
-    storage_account_name = "stacuser1" #change here
+    resource_group_name  = "rg-user1"
+    storage_account_name = "stacuser1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -29,4 +29,5 @@ module "keyvault" {
     name = "rg-user1"
   }
 }
+
 
