a091c6c24e852fd07db983b477d08a93895c5d7d
Avorlen <48469505+Avorlen@users.noreply.github.com>
2026-04-16 19:15:25 +0200

    Update Azure backend resource group and storage account

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "example-resources" #change here
-    storage_account_name = "tfstorage123dominik" #change here
+    resource_group_name  = "rg-user1" #change here
+    storage_account_name = "stacuser1" #change here
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
